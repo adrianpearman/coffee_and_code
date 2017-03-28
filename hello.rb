fc16b0b101e159615039d641c7809d3d4a27bf09
Adrian Pearman <adrianpearman12@gmail.com>
2017-03-28 09:50:33 -0400

    add response

--- hello.rb
+++ hello.rb
@@ -1,1 +1,11 @@
-Good Morning!
+#Good Morning!
+ puts "Out of 10, how are you this morning?"
+ response = gets.to_i
+
+ if response >= 7.5
+   puts "thats amazing!"
+ elsif response <= 2.5
+   puts "oh...its almost friday though!"
+ else
+   puts "get a coffee, it'll get better!"
+ end
